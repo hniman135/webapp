9a0db6879474029fc2fc40f564f33e3d2dc5b4ce
hniman135 <20521594@gm.uit.edu.vn>
2023-12-08 17:49:45 +0700

    .

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template , request , jsonify
+from flask import Flask, render_template , request , jsonify, send_file
 from PIL import Image
 import os , io , sys
 import numpy as np 
 import cv2
 import base64
 from yolo_detection_images import runModel
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -35,6 +36,16 @@ def mask_image():
 def test():
 	print("log: got at test" , file=sys.stderr)
 	return jsonify({'status':'succces'})
+
+@app.route('/get_doc_content')
+def get_doc_content():
+    # Đường dẫn đến tệp doc.txt, hãy đảm bảo rằng đường dẫn là chính xác
+    doc_path = 'D:\Learning\webapp\detect_result.txt'
+    with open(doc_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    parts = content.split()
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    return {'img_name': parts[0], 'ID': parts[1], 'time': current_time}
 
 @app.route('/')
 def home():
